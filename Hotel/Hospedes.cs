5497928bfb6cd9186cd294353b274b6cc29c0e4d
PorTTela <thiago.portela@ymail.com>
2021-06-24 14:23:57 -0300

    Adicionar arquivos de projeto.

---
+++ Hotel/Hospedes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel
+{
+    class Hospedes
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ Hotel/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Hospedes[] vect = new Hospedes[10];
+
+            Console.Write("Quantos alunos são?");
+            int alunos = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < alunos; i++)
+            {
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                vect[quarto] = new Hospedes { Nome = nome, Email = email };
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Relátorio de Quartos Alugados:");
+            for (int i=0; i<10; i++)
+            {
+                if(vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i].Nome + ", " + vect[i].Email);
+                }
+            }
+
+
+
+        }
+    }
+}
